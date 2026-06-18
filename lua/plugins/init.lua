575fa50ed39fac60c6f05804da64f6ab084ef4e0
rodrinac <jose.rs.inacio@gmail.com>
2026-06-18 15:52:11 +0100

    Modularize config into lua/{config,ui,plugins} structure

---
+++ lua/plugins/init.lua
@@ -0,0 +1,24 @@
+-- ============================================================================
+-- PLUGINS (vim.pack)
+-- ============================================================================
+vim.pack.add({"https://www.github.com/lewis6991/gitsigns.nvim", "https://www.github.com/echasnovski/mini.nvim",
+              "https://www.github.com/ibhagwan/fzf-lua", "https://www.github.com/nvim-tree/nvim-tree.lua", {
+    src = "https://github.com/nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    build = ":TSUpdate"
+}, -- Language Server Protocols
+"https://www.github.com/neovim/nvim-lspconfig", "https://github.com/mason-org/mason.nvim",
+              "https://github.com/creativenull/efmls-configs-nvim", {
+    src = "https://github.com/saghen/blink.cmp",
+    version = vim.version.range("1.*")
+}, "https://github.com/L3MON4D3/LuaSnip"})
+
+-- Plugin configurations (order matters: completion before lsp).
+require("plugins.treesitter")
+require("plugins.explorer")
+require("plugins.fzf")
+require("plugins.mini")
+require("ui.dashboard")
+require("plugins.gitsigns")
+require("plugins.completion")
+require("plugins.lsp")
---
+++ lua/ui/theme.lua
@@ -0,0 +1,21 @@
+-- ============================================================================
+-- THEME & TRANSPARENCY
+-- ============================================================================
+vim.opt.termguicolors = true
+vim.cmd.colorscheme("habamax")
+
+local function set_transparent() -- set UI component to transparent
+    local groups = {"Normal", "NormalNC", "EndOfBuffer", "NormalFloat", "FloatBorder", "SignColumn", "StatusLine",
+                    "StatusLineNC", "TabLine", "TabLineFill", "TabLineSel", "ColorColumn"}
+    for _, g in ipairs(groups) do
+        vim.api.nvim_set_hl(0, g, {
+            bg = "none"
+        })
+    end
+    vim.api.nvim_set_hl(0, "TabLineFill", {
+        bg = "none",
+        fg = "#767676"
+    })
+end
+
+set_transparent()
